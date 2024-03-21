afc89f70c0138164a79b9f0cb6e20f23c10c28e0
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-03-21 16:03:21 +0100

    Database connected through mongoose, data model created and some CRUD implementation

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Book = require("./models/Book");
+
+mongoose
+  .connect(
+    "mongodb+srv://new_user_1:GG6NL2DQWZ6wrP55@clusterforbooksapp.7ka29ua.mongodb.net/?retryWrites=true&w=majority&appName=ClusterForBooksApp",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Successfully connected to MongoDB!"))
+  .catch(() => console.log("Failed to connect to MongoDB!"));
 
 const app = express();
 
@@ -17,26 +27,41 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use("/api/books", (req, res, next) => {
-  const books = [
-    {
-      iserId: "1",
-      title: "The Lightning Thief",
-      author: "Rick Riordan",
-      imageUrl:
-        "https://www.amazon.com/Lightning-Thief-Percy-Jackson-Olympians/dp/0786838655",
-      year: "2005",
-      genre: "fantasy",
-      ratings: [
-        {
-          userId: "1",
-          grade: 5,
-        },
-      ],
-      averageRating: 4.5,
-    },
-  ];
-  res.status(200).json(books);
+app.get("/api/books", (req, res, next) => {
+  Book.find()
+    .then((books) => {
+      res.status(200).json(books);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+});
+
+app.post("/api/books", (req, res, next) => {
+  const book = new Book({
+    userId: "1",
+    title: "The Lightning Thief",
+    author: "Rick Riordan",
+    imageUrl:
+      "https://www.amazon.com/Lightning-Thief-Percy-Jackson-Olympians/dp/0786838655",
+    year: "2005",
+    genre: "fantasy",
+    ratings: [
+      {
+        userId: "1",
+        grade: 5,
+      },
+    ],
+    averageRating: 4.5,
+  });
+  book
+    .save()
+    .then(() => {
+      res.status(201).json(book);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 });
 
 module.exports = app;

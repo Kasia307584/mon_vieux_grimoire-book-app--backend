db34c76b64cfcf1f3e637b1f7cb78ffa5408afeb
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-03-25 13:00:13 +0100

    Comments removed

--- controllers/book.js
+++ controllers/book.js
@@ -1,15 +1,9 @@
-// const Book = require("../models/Book");
 const bookService = require("../services/bookService");
 
 exports.createBook = (req, res, next) => {
   delete req.body._id;
   bookService
     .createBook(req.body)
-    //   const book = new Book({
-    //     ...req.body,
-    //   });
-    //   book
-    //     .save()
     .then((book) => {
       res.status(201).json(book);
     })
@@ -19,7 +13,6 @@ exports.createBook = (req, res, next) => {
 };
 
 exports.getAllBooks = (req, res, next) => {
-  // Book.find()
   bookService
     .getAllBooks()
     .then((books) => {
@@ -31,7 +24,6 @@ exports.getAllBooks = (req, res, next) => {
 };
 
 exports.getOneBook = (req, res, next) => {
-  //   Book.findById(req.params.id)
   bookService
     .getOneBook(req.params.id)
     .then((book) => {
@@ -43,7 +35,6 @@ exports.getOneBook = (req, res, next) => {
 };
 
 exports.modifyBook = (req, res, next) => {
-  //   Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
   bookService
     .modifyBook(req.params.id, req.body)
     .then(() => res.status(200).json({ message: "Book updated!" }))
@@ -51,15 +42,8 @@ exports.modifyBook = (req, res, next) => {
 };
 
 exports.deleteBook = (req, res, next) => {
-  //   Book.findById(req.params.id)
-  // .then((book) => {
-  //   Book.deleteOne({ _id: req.params.id })
   bookService
     .deleteBook(req.params.id)
     .then(() => res.status(200).json({ message: "Book deleted!" }))
     .catch((error) => res.status(400).json({ error }));
-  // })
-  // .catch((error) => {
-  //   res.status(401).json({ error });
-  // });
 };

dede3f31b254f026280f6e7ad5198f122d7f9c48
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-03-26 23:21:21 +0100

    Signup function

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,16 @@
 const userService = require("../services/user");
+const bcrypt = require("bcrypt");
 
-exports.signup = (req, res, next) => {};
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(userData.password, 10)
+    .then((hash) => {
+      userService
+        .createUser(req.body, hash)
+        .then(() => res.status(201).json({ message: "User created!" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
 exports.login = (req, res, next) => {};
--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,18 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-exports.createUser = () => {};
+exports.createUser = (userData) => {
+  bcrypt
+    .hash(userData.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: userData.email,
+        password: hash,
+      });
+      return user.save();
+    })
+    .catch((error) => {
+      return error;
+    });
+};
 exports.findUser = () => {};

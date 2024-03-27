e5a9a1db5e073e94fe53a76785fe1596d2e44369
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-03-27 17:14:22 +0100

    Signup update (userService)

--- services/userService.js
+++ services/userService.js
@@ -1,18 +1,10 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
 
-exports.createUser = (userData) => {
-  bcrypt
-    .hash(userData.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: userData.email,
-        password: hash,
-      });
-      return user.save();
-    })
-    .catch((error) => {
-      return error;
-    });
+exports.createUser = (userData, hash) => {
+  const user = new User({
+    email: userData.email,
+    password: hash,
+  });
+  return user.save();
 };
 exports.findUser = () => {};

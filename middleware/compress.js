50ce7e855db0d0461b56725975c6e2091aed8f78
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-04-10 20:22:20 +0200

    Condition added and middlewares order changed
    To consider book modification that does not involve file update

--- middleware/compress.js
+++ middleware/compress.js
@@ -2,17 +2,19 @@ const sharp = require("sharp");
 
 module.exports = (req, res, next) => {
   const file = req.file;
-  const name = file.originalname.split(" ").join("_");
+  const name = file?.originalname.split(" ").join("_");
   const ref = `${name}${Date.now()}.webp`;
 
-  sharp(file.buffer)
-    .webp({ quality: 20 })
-    .toFile("images/" + ref)
-    .then(() => {
-      req.image = { ref: ref };
-      next();
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+  file
+    ? sharp(file.buffer)
+        .webp({ quality: 20 })
+        .toFile("images/" + ref)
+        .then(() => {
+          req.image = { ref: ref };
+          next();
+        })
+        .catch((error) => {
+          res.status(500).json({ error });
+        })
+    : null;
 };
--- routes/book.js
+++ routes/book.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/", auth, multer, compress, bookCtrl.createBook);
 router.get("/", bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getOneBook);
-router.put("/:id", auth, multer, compress, bookCtrl.modifyBook);
+router.put("/:id", auth, bookCtrl.modifyBook, multer, compress);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;

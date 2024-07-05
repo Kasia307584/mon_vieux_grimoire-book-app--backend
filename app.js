6856608dfd2f309551265cf0cbdfd9e2738437f7
PPRB <loic@beebs.app>
2024-07-05 14:21:10 +0200

    remove some routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const mongoose = require("mongoose");
 // const bookRoutes = require("./routes/book");
-const helloRoutes = require("./routes/hello");
+// const helloRoutes = require("./routes/hello");
 // const userRoutes = require("./routes/user");
 // const path = require("path");
 
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // app.use("/api/books", bookRoutes);
-app.use("/api/hello", helloRoutes);
+app.get('/', (req, res) => res.send('Home Page Route'));
 // app.use("/api/auth", userRoutes);
 // app.use("/images", express.static(path.join(__dirname, "images")));
 
